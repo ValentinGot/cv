0d6638330d765163ad607d34643e39c0b0d2f7e5
Valentin GOT <gotvalentin@gmail.com>
2018-04-14 22:31:07 +0200

    feat(pwa): make it progressive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppComponent } from './app.component';
-
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
-    BrowserModule.withServerTransition({ appId: 'valentin-got-cv' })
+    BrowserModule.withServerTransition({ appId: 'valentin-got-cv' }),
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   declarations: [
     AppComponent
