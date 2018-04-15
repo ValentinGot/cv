a44dd8a021bbac7ad15c171570f5c884bd4635f8
Valentin GOT <gotvalentin@gmail.com>
2018-04-15 11:48:41 +0200

    chore(shared): init a shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
-import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   imports: [
     BrowserModule.withServerTransition({ appId: 'valentin-got-cv' }),
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    SharedModule
   ],
   declarations: [
     AppComponent
