28a3de8b99f225fc014a31f4d6ca2ef439c1b55e
Valentin GOT <gotvalentin@gmail.com>
2018-04-25 23:37:07 +0200

    chore(app): add service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-// import { ServiceWorkerModule } from '@angular/service-worker';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { TransferHttpCacheModule } from '@nguniversal/common';
 
-// import { environment } from '../environments/environment';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   imports: [
     BrowserModule.withServerTransition({ appId: 'valentin-got-cv' }),
-    // ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     TransferHttpCacheModule,
     NoopAnimationsModule,
     SharedModule
