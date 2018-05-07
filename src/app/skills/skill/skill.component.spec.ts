c61d7f291f58d20d47623c229e2d3fe10655cdae
Valentin GOT <gotvalentin@gmail.com>
2018-05-07 14:26:58 +0200

    test(skill): test skill component

---
+++ src/app/skills/skill/skill.component.spec.ts
@@ -0,0 +1,66 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { MatIconModule, MatTooltipModule } from '@angular/material';
+
+import { SkillComponent } from './skill.component';
+import { Skill } from '../skill.model';
+
+describe ('SkillComponent', () => {
+  let component: SkillComponent;
+  let fixture: ComponentFixture<SkillComponent>;
+
+  beforeEach (async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        MatIconModule,
+        MatTooltipModule
+      ],
+      declarations: [ SkillComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach (() => {
+    fixture = TestBed.createComponent(SkillComponent);
+    component = fixture.componentInstance;
+  });
+
+  it (`should throw an error if the 'skill' input isn't present`, () => {
+    expect(() => fixture.detectChanges()).toThrow(new Error(`'skill' input is required`));
+  });
+
+  it (`should throw an error if the 'skill' input isn't of type Skill`, () => {
+    component.skill = {} as Skill;
+
+    expect(() => fixture.detectChanges()).toThrow(new TypeError(`'skill' input should be of type Skill`));
+  });
+
+  it ('should show the user info', () => {
+    const skill = new Skill();
+    skill.icon = 'skill:javascript';
+    skill.name = 'JavaScript';
+    skill.level = 9.5;
+    skill.main = true;
+    component.skill = skill;
+    fixture.detectChanges();
+
+    // -----------------------------------
+
+    const iconEl = fixture.debugElement.query(By.css('.mat-icon')).nativeElement;
+
+    expect(iconEl.getAttribute('ng-reflect-svg-icon')).toEqual('skill:javascript');
+
+    // -----------------------------------
+
+    const nameEl = fixture.debugElement.query(By.css('[data-info="name"]')).nativeElement;
+
+    expect(nameEl.textContent).toEqual('JavaScript');
+
+    // -----------------------------------
+
+    const levelEl = fixture.debugElement.query(By.css('[data-info="level"] > span')).nativeElement;
+
+    expect(levelEl.style.width).toEqual('95%');
+  });
+
+});
