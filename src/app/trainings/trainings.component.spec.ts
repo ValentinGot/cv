64d3a439323ebe890292058dadcc60e8981cb107
Valentin GOT <gotvalentin@gmail.com>
2018-06-10 20:16:26 +0200

    test(trainings): test component

--- src/app/trainings/trainings.component.spec.ts
+++ src/app/trainings/trainings.component.spec.ts
@@ -1,25 +1,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material';
+import { By } from '@angular/platform-browser';
 
 import { TrainingsComponent } from './trainings.component';
+import { TrainingComponent } from './training/training.component';
+import { Training } from './training.model';
 
 describe('TrainingsComponent', () => {
   let component: TrainingsComponent;
   let fixture: ComponentFixture<TrainingsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach (async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TrainingsComponent ]
+      imports: [
+        MatIconModule
+      ],
+      declarations: [
+        TrainingsComponent,
+        TrainingComponent
+      ]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach (() => {
     fixture = TestBed.createComponent(TrainingsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it (`should generate an empty list of trainings by default`, () => {
+    fixture.detectChanges();
+
+    const trainingEls = fixture.debugElement.queryAll(By.css('cv-training'));
+
+    expect(trainingEls.length).toEqual(0);
   });
+
+  it (`should generate a list of trainings`, () => {
+    const training = new Training();
+    training.name = 'Unagi';
+    training.year = 2005;
+    component.trainings = [ training ];
+    fixture.detectChanges();
+
+    const trainingEls = fixture.debugElement.queryAll(By.css('cv-training'));
+
+    expect(trainingEls.length).toEqual(1);
+  });
+
 });
