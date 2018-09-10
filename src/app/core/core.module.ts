74597ddd9301158ead932de19dca611ed646bf98
Valentin GOT <gotvalentin@gmail.com>
2018-09-10 17:33:53 +0200

    style(not-found): styling for the 404 page (not responsive for now)

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { NotFoundComponent } from './not-found/not-found.component';
-import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     TranslateModule.forChild()
   ],
   declarations: [
